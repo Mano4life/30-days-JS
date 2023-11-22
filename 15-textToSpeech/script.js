c0025a67bdf8674a73921afad497b571134ac0d9
Momen <132298128+Mano4life@users.noreply.github.com>
2023-11-23 01:11:18 +0200

    no15

---
+++ 15-textToSpeech/script.js
@@ -0,0 +1,33 @@
+let txt = document.getElementById("txt");
+let playBtn = document.getElementById("play-btn");
+let select = document.getElementById("select");
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let x = 0;
+
+
+// playing voice by clicking the play button
+playBtn.addEventListener("click", () => {
+    const testText = "hello, this is a test"
+    speech.text = testText;
+    // speech.text = txt.value;
+    console.log(x++);
+    console.log(window.speechSynthesis.getVoices());
+    window.speechSynthesis.speak(speech);
+});
+
+// capturing device voices and adding them to select.
+window.speechSynthesis.onvoiceschanged = () => {
+  // capturing voices on device.
+  voices = window.speechSynthesis.getVoices();
+  // default voice.
+  speech.voice = voices[0];
+  // adding them to select.
+  voices.forEach((voice, i) => (select.options[i] = new Option(voice.name, i)));
+};
+
+// changing the voice based on user selection.
+select.addEventListener("change", () => {
+    speech.voice = voices[select.value];
+})
